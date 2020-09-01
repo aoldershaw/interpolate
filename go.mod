191833edfa89d3073287fd7d1837946795cc9d85
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-09-01 13:48:20 -0400

    initial commit
    Add types for Var and interpolatable String. Note that String is special
    cased from any other type that gets generated since it can have Vars
    interspersed within it (whereas other types are either a Var or their
    actual value)
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/aoldershaw/interpolate
+
+go 1.15
+
+require (
+	github.com/fatih/structtag v1.2.0
+	github.com/hashicorp/go-multierror v1.1.0
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/onsi/gomega v1.10.1
+	golang.org/x/tools v0.0.0-20200831203904-5a2aa26beb65
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+)
---
+++ types.go
@@ -0,0 +1,103 @@
+package interpolate
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+)
+
+var (
+	// TODO: does this need to match regex in concourse/concourse?
+	// TODO: this will allow "((" inside a "((...))"
+	interpolationRegex         = regexp.MustCompile(`\(\(.+\)\)`)
+	interpolationAnchoredRegex = regexp.MustCompile("\\A" + interpolationRegex.String() + "\\z")
+)
+
+var (
+	errAssignedNotAVar             = fmt.Errorf("assigned value is not a var reference")
+	errInvalidTypeForInterpolation = func(t reflect.Type) error {
+		return fmt.Errorf("cannot interpolate %s into a string (only strings, numbers, and bools are supported)", t.Kind().String())
+	}
+)
+
+type Resolver interface {
+	Resolve(varName string) (interface{}, error)
+}
+
+type String string
+
+func (s String) Interpolate(resolver Resolver) (string, error) {
+	if interpolationAnchoredRegex.MatchString(string(s)) {
+		var dst string
+		if err := Var(s).InterpolateInto(resolver, &dst); err != nil {
+			return "", err
+		}
+		return dst, nil
+	}
+	var merr error
+	interpolated := interpolationRegex.ReplaceAllStringFunc(string(s), func(name string) string {
+		name = stripParens(name)
+
+		var val interface{}
+		if err := Var(name).InterpolateInto(resolver, &val); err != nil {
+			merr = multierror.Append(merr, err)
+			return name
+		}
+
+		switch val := val.(type) {
+		case string, json.Number, bool:
+			return fmt.Sprint(val)
+		default:
+			merr = multierror.Append(merr, errInvalidTypeForInterpolation(reflect.TypeOf(val)))
+			return name
+		}
+	})
+
+	return interpolated, merr
+}
+
+type Var string
+
+func (v *Var) UnmarshalJSON(data []byte) error {
+	var dst string
+	if err := json.Unmarshal(data, &dst); err != nil {
+		return err
+	}
+	if !interpolationAnchoredRegex.MatchString(dst) {
+		return errAssignedNotAVar
+	}
+	*v = Var(stripParens(dst))
+	return nil
+}
+
+func (v Var) MarshalJSON() ([]byte, error) {
+	return json.Marshal("((" + string(v) + "))")
+}
+
+func (v Var) InterpolateInto(resolver Resolver, dst interface{}) error {
+	val, err := resolver.Resolve(string(v))
+	if err != nil {
+		return err
+	}
+	payload, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+	dec := json.NewDecoder(bytes.NewReader(payload))
+	dec.UseNumber()
+	if err := dec.Decode(&dst); err != nil {
+		return err
+	}
+	return nil
+}
+
+func stripParens(name string) string {
+	name = strings.TrimPrefix(name, "((")
+	name = strings.TrimSuffix(name, "))")
+	return name
+}
